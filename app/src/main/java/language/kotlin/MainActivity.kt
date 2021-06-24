dac53d4f13a6e75aac0a46a2a9e3dc92dd5aa19f
kimminkyu <kmk50009@naver.com>
2021-06-24 23:08:51 +0900

    kotlin initial commit

---
+++ app/src/main/java/language/kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package language.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
